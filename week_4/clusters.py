8a24f1f30097a73fffc1b67665861cfa9fef7f04
mary <goodkarma000>
2019-09-13 20:43:52 +0300

    week 4 clusters

--- week_4/clusters.py
+++ week_4/clusters.py
@@ -4,8 +4,12 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from scipy.cluster.hierarchy import fcluster, dendrogram, linkage
+from sklearn.cluster import DBSCAN
 
-def silhouette(X):
+def silhouette(df):
+    df_dummy = pd.get_dummies(df)
+    # print(df.info())
+    X = df_dummy.values
     scores = []
     for n in range(2,20):
         kmeans = KMeans(n_clusters=n, random_state=123)
@@ -14,14 +18,47 @@ def silhouette(X):
         # preds = kmeans.fit_predict(X)
         scores.append(silhouette_score(X, labels))
 
-    return round(max(scores),2)
+    return max(scores)
 
-def aglomerate(X):
+def aglomerate(df):
+    df_dummy = pd.get_dummies(df)
+    X = df_dummy.values
+    # X = df_dummy.iloc[:, 1:].values
+    # X = (X - X.mean(axis=0)) / X.std(axis=0)
     Z = linkage(X, method='average', metric='cosine')
-    fcluster(Z, t=5, criterion='maxclust')
-    dendrogram(Z)
+    label = fcluster(Z, t=5, criterion='maxclust')
+    df.loc[:, 'label'] = label
+    for i, group in df.groupby('label'):
+        print('=' *10)
+        print('cluster{}'.format(i))
+        print(len(group))
+        print(group['What is your gender?'].value_counts(normalize=True))
+
     plt.show()
     # return dendrogram(Z)
+
+def dbscen_ex(df):
+    # df = df.drop(labels=df.columns[0], axis=1)
+    df_dummy = pd.get_dummies(df)
+    # print(df.info())
+    X = df_dummy.values
+    df.replace('-', np.nan, inplace=True)
+    print(len(df.index))
+    df1 = df.dropna()
+    print(f'withoun NaN = {len(df1.index)}')
+    e_m = np.linspace(0.1, 1.0, num = 10)
+    for e in e_m:
+        db = DBSCAN(eps=e, min_samples=20, metric='cosine').fit(X)
+        label = db.labels_
+        df.loc[:, 'label'] = label
+        # print(label)
+        for i, group in df.groupby('label'):
+            print('=' * 10)
+            print('cluster{}'.format(i))
+            print('eps={}'.format(e))
+            print(group)
+
+
 
 
 
@@ -31,11 +68,9 @@ def main():
     df = pd.read_csv('weather-check.csv')
     df = df.drop(labels=df.columns[0], axis=1)
     df = df.drop(labels=df.columns[2], axis=1)
-    df_dummy = pd.get_dummies(df)
-    print(df.head())
-    X = df_dummy.values
-    # print(silhouette(X))
-    print(aglomerate(X))
+    print(silhouette(df))
+    print(aglomerate(df))
+    print(dbscen_ex(df))
 
 if __name__ == '__main__':
     main()
